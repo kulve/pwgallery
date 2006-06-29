5ee7559edd0f4eb458d8e71417a8acb7cd339a5a
kulve <kulve@4fe92e11-3017-0410-8d35-f85eb56c5e15>
2006-06-29 19:32:11 +0000

    Start
    git-svn-id: http://tuomas.kulve.fi/svn/pwgallery/trunk@1 4fe92e11-3017-0410-8d35-f85eb56c5e15

---
+++ src/main.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2006 Tuomas Kulve <tuomas@kulve.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ * USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "glade/interface.h"
+#include "glade/support.h"
+
+
+int
+main (int argc, char *argv[])
+{
+    GtkWidget *mainwindow;
+    
+#ifdef ENABLE_NLS
+    bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+    bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+    textdomain (GETTEXT_PACKAGE);
+#endif
+    
+    gtk_set_locale ();
+    gtk_init (&argc, &argv);
+    
+    add_pixmap_directory (PACKAGE_DATA_DIR "/" PACKAGE "/pixmaps");
+    
+    /*
+     * The following code was added by Glade to create one of each component
+     * (except popup menus), just so that you see something after building
+     * the project. Delete any components that you don't want shown initially.
+     */
+    mainwindow = create_mainwindow ();
+    gtk_widget_show (mainwindow);
+    
+    gtk_main ();
+    return 0;
+}
+
+
+/* Emacs indentatation information
+   Local Variables:
+   indent-tabs-mode:nil
+   tab-width:4
+   c-set-offset:4
+   c-basic-offset:4
+   End: 
+*/
