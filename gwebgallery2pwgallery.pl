5aa2733f88e5df63248223a536f0f57824ce0bc7
kulve <kulve@4fe92e11-3017-0410-8d35-f85eb56c5e15>
2007-06-17 18:11:52 +0000

    gwebgallery2pwgallery: Initial fixes
    git-svn-id: http://tuomas.kulve.fi/svn/pwgallery/trunk@64 4fe92e11-3017-0410-8d35-f85eb56c5e15

--- gwebgallery2pwgallery.pl
+++ gwebgallery2pwgallery.pl
@@ -5,6 +5,12 @@ use strict;
 my %GALLERY;       # hash for data
 my $img_count = 0; # image counter
 
+my $TEMPL_INDEX    = "file:///home/aino/.pwgallery/templates/aino_index.shtml";
+my $TEMPL_INDEXIMG = "file:///home/aino/.pwgallery/templates/aino_indeximg.shtml";
+my $TEMPL_INDEXGEN = "file:///home/aino/.pwgallery/templates/pwg_indexgen.html";
+my $TEMPL_IMAGE    = "file:///home/aino/.pwgallery/templates/aino_image-onesize.shtml";
+my $TEMPL_GEN      = "file:///home/aino/.pwgallery/templates/pwg_generic.html";
+
 if (not defined $ARGV[0] or not defined $ARGV[1]) {
   print "Usage: $0 gwebgalleryfile pwgalleryfile.xml\n";
   exit(1);
@@ -112,65 +118,79 @@ sub parse_image {
   $GALLERY{"IMG"}->{$img_count}->{"desc"} = $1
 	  if ($i =~  m/<DESCRIPTION>(.*)<\/DESCRIPTION>/s);
 
+  # Remove "Add text"
+  $GALLERY{"IMG"}->{$img_count}->{"desc"} =~ s/Add text//g;
+
   # strip
-   $GALLERY{"IMG"}->{$img_count}->{"desc"} =~ s/^\s+|\s+$//g;
+  $GALLERY{"IMG"}->{$img_count}->{"desc"} =~ s/^\s+|\s+$//g;
+
+  # Convert "<br>" to newline
+  $GALLERY{"IMG"}->{$img_count}->{"desc"} =~ s/<br>/\n/g;
+
+  # &deg; -> &#xB0;
+  $GALLERY{"IMG"}->{$img_count}->{"desc"} =~ s/\&deg;/\&\#xB0;/g;
+
 }
 
 
 
 sub print_pwgallery {
 
+  open (OUTPUT, ">$PWG")
+	  or die ("Failed to open $PWG: $!");
+
   print "Gallery: $GALLERY{MAIN}->{name}\n";
 
-  print "<?xml version=\"1.0\"?>\n";
-  print "<pwgallery>\n";
-  print "  <settings>\n";
+  print OUTPUT "<?xml version=\"1.0\"?>\n";
+  print OUTPUT "<pwgallery>\n";
+  print OUTPUT "  <settings>\n";
 
-  print "    <version>$GALLERY{MAIN}->{version}</version>\n";
-  print "    <name>$GALLERY{MAIN}->{name}</name>\n";
+  print OUTPUT "    <version>$GALLERY{MAIN}->{version}</version>\n";
+  print OUTPUT "    <name>$GALLERY{MAIN}->{name}</name>\n";
 
   my ($output_dir, $dir_name) = get_dirs($GALLERY{"MAIN"}->{"outputdir"});
 
-  print "    <dir_name>$dir_name</dir_name>\n";
-  print "    <output_dir>file://$output_dir</output_dir>\n";
+  print OUTPUT "    <dir_name>$dir_name</dir_name>\n";
+  print OUTPUT "    <output_dir>file://$output_dir</output_dir>\n";
 
-  print "    <page_gen_prog></page_gen_prog>\n";
-  print "    <templ_index></templ_index>\n";
-  print "    <templ_indeximg></templ_indeximg>\n";
-  print "    <templ_indexgen></templ_indexgen>\n";
-  print "    <templ_image></templ_image>\n";
-  print "    <templ_gen></templ_gen>\n";
-  print "    <page_gen>1</page_gen>\n";
+  print OUTPUT "    <page_gen_prog></page_gen_prog>\n";
+  print OUTPUT "    <templ_index>$TEMPL_INDEX</templ_index>\n";
+  print OUTPUT "    <templ_indeximg>$TEMPL_INDEXIMG</templ_indeximg>\n";
+  print OUTPUT "    <templ_indexgen>$TEMPL_INDEXGEN</templ_indexgen>\n";
+  print OUTPUT "    <templ_image>$TEMPL_IMAGE</templ_image>\n";
+  print OUTPUT "    <templ_gen>$TEMPL_GEN</templ_gen>\n";
+  print OUTPUT "    <page_gen>1</page_gen>\n";
 
-  print "    <thumb_w>$GALLERY{MAIN}->{thumb_width}</thumb_w>\n";
-  print "    <image_h>$GALLERY{MAIN}->{image_height}</image_h>\n";
-  print "    <image_h2>0</image_h2>\n";
-  print "    <image_h3>0</image_h3>\n";
-  print "    <image_h4>0</image_h4>\n";
+  print OUTPUT "    <thumb_w>$GALLERY{MAIN}->{thumb_width}</thumb_w>\n";
+  print OUTPUT "    <image_h>$GALLERY{MAIN}->{image_height}</image_h>\n";
+  print OUTPUT "    <image_h2>0</image_h2>\n";
+  print OUTPUT "    <image_h3>0</image_h3>\n";
+  print OUTPUT "    <image_h4>0</image_h4>\n";
 
-  print "    <edited>false</edited>\n";
-  print "    <remove_exif>true</remove_exif>\n";
-  print "    <rename>false</rename>\n";
-  print "  </settings>\n";
-  print "  <pages>\n";
+  print OUTPUT "    <edited>false</edited>\n";
+  print OUTPUT "    <remove_exif>true</remove_exif>\n";
+  print OUTPUT "    <rename>false</rename>\n";
+  print OUTPUT "  </settings>\n";
+  print OUTPUT "  <pages>\n";
 
-  foreach my $i (keys %{$GALLERY{"IMG"}}) {
-	print "    <image>\n";
-	print "      <settings>\n";
-	print "        <text>$GALLERY{IMG}->{$i}->{desc}</text>\n";
-	print "        <uri>file://$GALLERY{IMG}->{$i}->{filename}</uri>\n";
-	print "        <gamma>$GALLERY{IMG}->{$i}->{gamma}</gamma>\n";
-	print "        <rotate>$GALLERY{IMG}->{$i}->{rotation}</rotate>\n";
+  foreach my $i (sort { $a <=> $b } keys %{$GALLERY{"IMG"}}) {
+	print OUTPUT "    <image>\n";
+	print OUTPUT "      <settings>\n";
+	print OUTPUT "        <text>$GALLERY{IMG}->{$i}->{desc}</text>\n";
+	print OUTPUT "        <uri>file://$GALLERY{IMG}->{$i}->{filename}</uri>\n";
+	print OUTPUT "        <gamma>$GALLERY{IMG}->{$i}->{gamma}</gamma>\n";
+	print OUTPUT "        <rotate>$GALLERY{IMG}->{$i}->{rotation}</rotate>\n";
 	my $nomodify = "false";
 	$nomodify = "true"
-		if ($GALLERY{"IMG"}->{$i}->{"nomodify"} == 1);
-	print "        <nomodify>$nomodify</nomodify>\n";
-	print "      </settings>\n";
-	print "    </image>\n";
+		if (defined $GALLERY{"IMG"}->{$i}->{"nomodify"} and 
+			$GALLERY{"IMG"}->{$i}->{"nomodify"} == 1);
+	print OUTPUT "        <nomodify>$nomodify</nomodify>\n";
+	print OUTPUT "      </settings>\n";
+	print OUTPUT "    </image>\n";
   }
 
-  print "  </pages>\n";
-  print "</pwgallery>\n";
+  print OUTPUT "  </pages>\n";
+  print OUTPUT "</pwgallery>\n";
 
 }
 
